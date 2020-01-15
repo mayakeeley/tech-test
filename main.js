c1567effe755227f7b70c6d38cc8dc06a73accaa
mayakeeley <maya.keeley@outlook.com>
2020-01-15 11:14:36 +0000

    found all mines

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ const createMinefield = (rows, columns) => {
 
   table = document.createElement("table");
 
-  for(let row=0; row < rows; row++) {
+  for (let row = 0; row < rows; row++) {
     tr = document.createElement("tr");
 
-    for(let column = 0; column < columns; column++) {
+    for (let column = 0; column < columns; column++) {
       td = document.createElement("td");
       td.id = `y${row}x${column}`;
       tr.appendChild(td);
@@ -24,14 +24,14 @@ createMinefield(15, 15);
 
 const generateMines = (rows, columns, mines) => {
   if(mines < rows * columns) {
-    let x=0;
-    while(x < mines) {
+    let x = 0;
+    while (x < mines) {
       let rowId = Math.floor(Math.random() * rows);
       let colId = Math.floor(Math.random() * columns);
       let cellId = `y${rowId}x${colId}`;
       let cell = document.getElementById(cellId);
 
-      if(cell.classList.contains("mine")) {
+      if (cell.classList.contains("mine")) {
         continue;
       } 
       cell.classList.add("mine");
@@ -44,3 +44,13 @@ const generateMines = (rows, columns, mines) => {
 }
 
 generateMines(15, 15, 30);
+
+const generateClues = (rows, columns) => {
+  let allMineCells = document.querySelectorAll(".mine");
+  let allMineIds = [];
+  allMineCells.forEach(cell => {
+    allMineIds.push(cell.id);
+  })
+}
+
+generateClues(15, 15);
